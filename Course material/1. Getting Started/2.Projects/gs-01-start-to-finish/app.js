e78a9177a30d26a8cfa728dba551c932a315536c
Asparuh Bambov <akbamboff@gmail.com>
2024-02-15 12:07:09 +0200

    first Vue project.

--- Course material/1. Getting Started/2.Projects/gs-01-start-to-finish/app.js
+++ Course material/1. Getting Started/2.Projects/gs-01-start-to-finish/app.js
@@ -1,10 +1,17 @@
 Vue.createApp({
   data() {
     return {
-      
-    }
-  }
-})
+      goals: [],
+      enteredValue: '',
+    };
+  },
+  methods: {
+    addGoal() {
+      this.goals.push(this.enteredValue);
+      this.enteredValue = '';
+    },
+  },
+}).mount('#app');
 
 // const addButtonEL = document.querySelector('button');
 // const listEl = document.querySelector('ul');
