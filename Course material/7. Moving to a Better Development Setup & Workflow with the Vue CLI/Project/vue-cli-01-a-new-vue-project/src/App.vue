8ec43e58007e3ce9a7906f687edc84cd66cb0d2e
Asparuh Bambov <akbamboff@gmail.com>
2024-03-06 13:27:01 +0200

    some changes.

---
+++ Course material/7. Moving to a Better Development Setup & Workflow with the Vue CLI/Project/vue-cli-01-a-new-vue-project/src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <h2>My Friends</h2>
+  <section>
+    <ul>
+      <friend-contact></friend-contact>
+      <friend-contact></friend-contact>
+    </ul>
+  </section>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      friends: [
+        {
+          id: 'tedo',
+          name: "Teodor Georgiev",
+          phone: '08935454544',
+          email: 'teodor@localhost.com'
+        },
+        {
+          id: 'vito',
+          name: "Vitorio Pavlov",
+          phone: '08769202022',
+          email: 'vitorio@localhost.com'
+        },
+      ]
+    }
+  },
+
+};
+</script>
---
+++ Course material/7. Moving to a Better Development Setup & Workflow with the Vue CLI/Project/vue-cli-01-a-new-vue-project/src/components/ FriendContact.vue
@@ -0,0 +1,34 @@
+<template>
+  <li>
+    <h2>{{ friend.name }}</h2>
+    <button @click="toggleDetails">
+      Show Details
+    </button>
+    <ul v-if="detailsAreVisible">
+      <li><strong>Phone:</strong>{{ friend.phone }}</li>
+      <li><strong>Email:</strong> {{ friend.email }}</li>
+    </ul>
+  </li>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      detailsAreVisible: false,
+      friend: {
+        id: 'tedo',
+        name: "Teodor Georgiev",
+        phone: '08935454544',
+        email: 'teodor@localhost.com',
+      }
+    }
+  },
+  methods: {
+    toggleDetails() {
+      this.detailsAreVisible = !this.detailsAreVisible;
+
+    }
+  }
+}
+</script>
