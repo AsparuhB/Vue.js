1db7e330ed634b57faf6a6e7bf520c5e2eea62c0
Asparuh Bambov <akbamboff@gmail.com>
2024-03-12 12:20:08 +0200

    finishing with the usage of provide/inject

--- Course material/7. Moving to a Better Development Setup & Workflow with the Vue CLI/Project/cmp-communication-08-a-potential-problem-starting-setup/src/App.vue
+++ Course material/7. Moving to a Better Development Setup & Workflow with the Vue CLI/Project/cmp-communication-08-a-potential-problem-starting-setup/src/App.vue
@@ -4,7 +4,7 @@
       :topic-title="activeTopic && activeTopic.title"
       :text="activeTopic && activeTopic.fullText"
     ></active-element>
-    <knowledge-base @select-topic="activateTopic"></knowledge-base>
+    <knowledge-base></knowledge-base>
   </div>
 </template>
 
@@ -32,15 +32,26 @@ export default {
       activeTopic: null,
     };
   },
-  provide(){
+  provide() {
     return {
-      topics: this.topics
-    }
-  }, 
+      topics: this.topics,
+      selectTopic: this.activateTopic
+    };
+  },
   methods: {
     activateTopic(topicId) {
       this.activeTopic = this.topics.find((topic) => topic.id === topicId);
     },
+  },
+  mounted() {
+    setTimeout(() => {
+      this.topics.push({
+        id: 'events',
+        title: 'Events',
+        description: 'Events are important in Vue',
+        fullText: 'Events allow you to trigger code on demand!',
+      });
+    }, 3000);
   },
 };
 </script>
