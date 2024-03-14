478fce0aacc89eeb116e84850d48ff6dd4d5da50
Asparuh Bambov <akbamboff@gmail.com>
2024-03-14 12:27:45 +0200

    Fragments

--- Course material/9. Diving Deeper Into Components/Project/cmp-adv-01-starting-setup/src/components/ManageGoals.vue
+++ Course material/9. Diving Deeper Into Components/Project/cmp-adv-01-starting-setup/src/components/ManageGoals.vue
@@ -1,20 +1,18 @@
 <template>
-  <div>
-    <h2>Manage Goals</h2>
-    <input type="text" ref="goal" />
-    <button @click="setGoal">Set Goal</button>
-    <teleport to="body">
+  <h2>Manage Goals</h2>
+  <input type="text" ref="goal" />
+  <button @click="setGoal">Set Goal</button>
+  <teleport to="body">
     <error-alert v-if="inputIsInvalid">
       <h2>Input is invalid!</h2>
       <p>Please enter at least a few characters....</p>
       <button @click="confirmError">Okay</button>
     </error-alert>
   </teleport>
-  </div>
 </template>
 
 <script>
-import ErrorAlert from './ErrorAlert.vue'
+import ErrorAlert from './ErrorAlert.vue';
 export default {
   components: {
     ErrorAlert,
@@ -32,8 +30,8 @@ export default {
       }
     },
     confirmError() {
-      this.inputIsInvalid = false
-    }
+      this.inputIsInvalid = false;
+    },
   },
 };
 </script>
