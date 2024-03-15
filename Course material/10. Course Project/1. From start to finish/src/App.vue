545dae81efc7725a9c46527f7dd64da5eacec71f
Asparuh Bambov <akbamboff@gmail.com>
2024-03-15 11:18:36 +0200

    changing stuff

---
+++ Course material/10. Course Project/1. From start to finish/src/App.vue
@@ -0,0 +1,38 @@
+<template>
+  <ul>
+    <ListItems v-for="recourse in storedRecourses" :key="recourse.id">
+      <template #heading> {{ recourse.title }}</template>
+      <template #desc> {{ recourse.description }}</template>
+      <template #link><a :href="recourse.link">{{ recourse.link }}</a></template>>
+    </ListItems>
+
+  </ul>
+</template>
+
+<script>
+import ListItems from './components/ListItems.vue'
+
+export default {
+  components: {
+    ListItems,
+  },
+  data() {
+    return {
+      storedRecourses: [
+        {
+          id: 'official-guide',
+          title: 'Official Guide',
+          description: 'The official Vue.js documentation.',
+          link: 'https://vuejs.org',
+        },
+        {
+          id: 'google',
+          title: 'Google',
+          description: 'Learn to google...',
+          link: 'https://google.com',
+        },
+      ],
+    };
+  },
+};
+</script>
---
+++ Course material/10. Course Project/1. From start to finish/src/components/ListItems.vue
@@ -0,0 +1,11 @@
+<template>
+  <li>
+    <h2><slot name="heading"></slot></h2>
+    <p><strong><slot name="desc"></slot></strong></p>
+    <slot name="link"></slot>
+  </li>
+</template>
+
+<script>
+export default {};
+</script>
