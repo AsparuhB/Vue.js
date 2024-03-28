850d79582e3194ccd25e599e2094742fec2053ec
Asparuh Bambov <akbamboff@gmail.com>
2024-03-28 10:58:31 +0200

    prettier

--- Course material/13. Routing/1. Routing Start to Finish/routing-01-starting-setup/src/main.js
+++ Course material/13. Routing/1. Routing Start to Finish/routing-01-starting-setup/src/main.js
@@ -1,25 +1,25 @@
 import { createApp } from 'vue';
 import { createRouter, createWebHistory } from 'vue-router';
 
-import App from './App.vue'
-import TeamsList from './components/teams/TeamsList.vue'
-import UsersList from './components/users/UsersList.vue'
-import TeamMembers from './components/teams/TeamMembers.vue'
+import App from './App.vue';
+import TeamsList from './components/teams/TeamsList.vue';
+import UsersList from './components/users/UsersList.vue';
+import TeamMembers from './components/teams/TeamMembers.vue';
 
 const router = createRouter({
   history: createWebHistory(),
   routes: [
     { path: '/teams', component: TeamsList }, // our-domain.com/teams => TeamsList
     { path: '/users', component: UsersList },
-    {path: '/teams/:teamId', component: TeamMembers, props: true},
-     // our-domain.com/users => UsersList
+    { path: '/teams/:teamId', component: TeamMembers, props: true },
+    // our-domain.com/users => UsersList
   ],
   linkActiveClass: 'active',
-  linkExactActiveClass: 'exact'
+  linkExactActiveClass: 'exact',
 });
 
 const app = createApp(App);
 
-app.use(router)
+app.use(router);
 
 app.mount('#app');
