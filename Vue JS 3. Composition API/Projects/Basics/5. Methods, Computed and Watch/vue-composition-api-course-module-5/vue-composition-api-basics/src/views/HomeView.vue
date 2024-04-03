db93948b2ac8d3e460ba33ef236cb7c3e857d550
Asparuh Bambov <akbamboff@gmail.com>
2024-04-03 12:54:45 +0300

    New files and movements

--- Vue JS 3. Composition API/Projects/Basics/5. Methods, Computed and Watch/vue-composition-api-course-module-5/vue-composition-api-basics/src/views/HomeView.vue
+++ Vue JS 3. Composition API/Projects/Basics/5. Methods, Computed and Watch/vue-composition-api-course-module-5/vue-composition-api-basics/src/views/HomeView.vue
@@ -1,7 +1,10 @@
 <template>
   <div class="home">
+    
     <h2>{{ appTitle }}</h2>
+
     <h3>{{ counterData.title }}:</h3>
+
     <div>
       <button @click="decreaseCounter(2)" class="btn">--</button>
       <button @click="decreaseCounter(1)" class="btn">-</button>
@@ -11,60 +14,77 @@
     </div>
 
     <p>This counter is {{ oddOrEven }}</p>
-    <p>{{ helloThere }}.</p>
 
     <div class="edit">
       <h4>Edit counter title:</h4>
-      <input v-model="counterData.title" type="text" />
+      <input v-model="counterData.title" type="text">
     </div>
+
   </div>
 </template>
 
 <script setup>
-import { reactive, computed, watch } from 'vue';
+import { reactive, computed, watch } from 'vue'
 
-const appTitle = 'My Amazing Counter App';
+const appTitle = 'My Ok Counter App'
 
 const counterData = reactive({
   count: 0,
-  title: 'My Counter',
-});
+  title: 'My Counter'
+})
 
 watch(() => counterData.count, (newCount) => {
   if (newCount === 20) {
-    alert("Way to go! You made it to 20!!")
+    alert('Way to go! You made it to 20!!')
   }
 })
 
 const oddOrEven = computed(() => {
-  if (counterData.count % 2 === 0) return 'even';
-  return 'odd';
-});
+  if (counterData.count % 2 === 0) return 'even'
+  return 'odd'
+})
 
-const increaseCounter = (amount, event) => {
-  counterData.count += amount;
-};
+const increaseCounter = (amount, e) => {
+  counterData.count += amount
+}
 
-const decreaseCounter = (amount) => {
-  counterData.count -= amount;
-};
-
-const helloThere = computed(() => {
-  if (counterData.count === 9) return 'Hello there';
-});
+const decreaseCounter = amount => {
+  counterData.count -= amount
+}
 </script>
+
+<!--
+<script>
+export default {
+  data() {
+    return {
+      count: 0
+    }
+  },
+  computed: {
+    myComputedProperty() {
+      // perform logic based on a data property
+      return 'my result'
+    }
+  },
+  watch: {
+    count(newCount, oldCount) {
+      if (newCount == 20) alert('asdfasd')
+    }
+  }
+}
+</script>
+-->
 
 <style>
 .home {
   text-align: center;
   padding: 20px;
 }
-.btn,
-.counter {
+.btn, .counter {
   font-size: 40px;
   margin: 10px;
 }
-
 .edit {
   margin-top: 60px;
 }
