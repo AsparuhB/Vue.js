7171754c11287b228d0f2f748eb525d346cb4b6c
Asparuh Bambov <akbamboff@gmail.com>
2024-04-12 10:08:55 +0300

    creating

--- Vue JS 3. Composition API/2.Noteballs project/vue-composition-api-noteballs/src/views/ViewNotes.vue
+++ Vue JS 3. Composition API/2.Noteballs project/vue-composition-api-noteballs/src/views/ViewNotes.vue
@@ -3,25 +3,28 @@
     <div class="card has-background-success-dark p-4 mb-5">
       <div class="field">
         <div class="control">
-          <textarea class="textarea" placeholder="Add a new note" />
+          <textarea
+            v-model="newNote"
+            class="textarea"
+            placeholder="Add a new note" />
         </div>
       </div>
 
       <div class="field is-grouped is-grouped-right">
         <div class="control">
-          <button class="button is-link has-background-success">
+          <button
+            @click="addNewNote"
+            :disabled="!newNote"
+            class="button is-link has-background-success">
             Add New Note
           </button>
         </div>
       </div>
     </div>
 
-    <div class="card mb-4" v-for="i in 3">
+    <div class="card mb-4" v-for="note in notes" :key="note.id">
       <div class="card-content">
-        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Magni quaerat
-        eaque suscipit reiciendis, obcaecati animi, hic, maiores sapiente
-        aliquid rem dolorum provident soluta. Molestiae consequuntur earum
-        libero dolor, nostrum eligendi?
+        {{ note.content }}
       </div>
       <footer class="card-footer">
         <a href="#" class="card-footer-item">Edit</a>
@@ -36,5 +39,26 @@
 import { ref } from 'vue';
 //notes
 
-const notes = ref()
+const newNote = ref('');
+
+const notes = ref([
+  {
+    id: 'id1',
+    content:
+      'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Magni quaera teaque suscipit reiciendis, obcaecati animi, hic, maiores sapiente aliquid rem dolorum provident soluta. Molestiae consequuntur earum libero dolor, nostrum eligendi?',
+  },
+  {
+    id: 'id2',
+    content: 'This is a shorter note! Woo!',
+  },
+]);
+
+const addNewNote = () => {
+  const note = {
+    id: new Date().toISOString(),
+    content: newNote.value,
+  };
+  notes.value.push(note);
+  newNote.value = ''
+};
 </script>
